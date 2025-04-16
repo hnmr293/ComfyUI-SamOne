f08a98d3f8fb8d170da92e22314f69c9a8642005
hnmr293 <hnmr293@gmail.com>
2025-04-16 10:37:46 +0900

    モデルへの入出力をとれるようにした

--- samone/sampler.py
+++ samone/sampler.py
@@ -23,8 +23,8 @@ class SamplerOne:
             },
         }
 
-    RETURN_TYPES = ("LATENT", "LATENT")
-    RETURN_NAMES = ("scaled_sample", "non_scaled_sample")
+    RETURN_TYPES = ("LATENT", "LATENT", "LATENT", "LATENT")
+    RETURN_NAMES = ("scaled_sample", "non_scaled_sample", "model_input", "model_output")
 
     FUNCTION = "sample"
     CATEGORY = "hnmr/samone"
@@ -54,6 +54,8 @@ class SamplerOne:
             raise ValueError("latent and latent_image must have the same dtype")
 
         sigmas = torch.tensor([sigma_from, sigma_to], device=noise.device, dtype=torch.float32)
+
+        # モデルへの入力をフックしてスケールしていない latent を取得する
 
         inner_model: BaseModel = guider.model_patcher.model
         model_sampling = inner_model.model_sampling
@@ -93,6 +95,19 @@ class SamplerOne:
         inner_model.process_latent_out = dummy_latent_out
         model_sampling.inverse_noise_scaling = dummy_noise_scaling_inv
 
+        # モデルへの入力をフックして、モデルへの入力とモデルからの出力を取得する
+        model_input = None
+        model_output = None
+        diffusion_model = inner_model.diffusion_model
+
+        def hook_forward(module, input, output):
+            # input := (xc, t, ...)
+            nonlocal model_input, model_output
+            model_input = input[0].clone().detach()
+            model_output = output.clone().detach()
+
+        hook_handle = diffusion_model.register_forward_hook(hook_forward)
+
         def dummy_callback(*args, **kwargs):
             pass
 
@@ -113,6 +128,7 @@ class SamplerOne:
                 model_sampling.noise_scaling = orig_noise_scaling
             inner_model.process_latent_out = orig_latent_out
             model_sampling.inverse_noise_scaling = orig_noise_scaling_inv
+            hook_handle.remove()
 
         samples = samples.to(comfy.model_management.intermediate_device())
 
@@ -122,4 +138,7 @@ class SamplerOne:
         non_scaled_out = latent.copy()
         non_scaled_out["samples"] = non_scaled_latent2
 
-        return (out, non_scaled_out)
+        model_input_ = {"samples": model_input}
+        model_output_ = {"samples": model_output}
+
+        return (out, non_scaled_out, model_input_, model_output_)
